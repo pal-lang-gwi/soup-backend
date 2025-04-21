a14e2e9b602d349075d18f78c085ae73f764173d
김여준 <kt65346337@gmail.com>
2025-04-21 17:35:54 +0900

    fix: 닉네임 중복 예외 및 전역 예외 처리 추가

--- src/main/java/com/palangwi/soup/exception/user/UserNotFoundException.java
+++ src/main/java/com/palangwi/soup/exception/user/UserNotFoundException.java
@@ -1,13 +1,15 @@
 package com.palangwi.soup.exception.user;
 
+import com.palangwi.soup.exception.BaseCustomException;
 import org.springframework.http.HttpStatus;
 
-public class UserNotFoundException extends RuntimeException {
-  @Override
-  public String getMessage() {
-    return UserExceptionMessage.USER_NOT_FOUND.getMessage();
-  }
-  public HttpStatus getStatus() {
-    return UserExceptionMessage.USER_NOT_FOUND.getStatus();
-  }
+public class UserNotFoundException extends BaseCustomException {
+    @Override
+    public String getMessage() {
+        return UserExceptionMessage.USER_NOT_FOUND.getMessage();
+    }
+
+    public HttpStatus getStatus() {
+        return UserExceptionMessage.USER_NOT_FOUND.getStatus();
+    }
 }
