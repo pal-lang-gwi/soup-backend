7194d3e8e4f3e5661996be1dcc01c983e44275eb
김여준 <kt65346337@gmail.com>
2025-04-22 23:11:31 +0900

    fix: Security 설정 및 구글 로그인 동작 확인

--- src/test/java/com/palangwi/soup/SoupApplicationTests.java
+++ src/test/java/com/palangwi/soup/SoupApplicationTests.java
@@ -2,9 +2,8 @@ package com.palangwi.soup;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 
-@Import(TestcontainersConfiguration.class)
+//@Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class SoupApplicationTests {
 
