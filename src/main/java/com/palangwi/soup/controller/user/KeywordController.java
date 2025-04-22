563bef830ffd82a99ce77c13de0988da02c4621c
유재광 <marmong9770@gmail.com>
2025-04-22 21:19:01 +0900

    feat: keyword domain init

---
+++ src/main/java/com/palangwi/soup/controller/user/KeywordController.java
@@ -0,0 +1,22 @@
+package com.palangwi.soup.controller.user;
+
+import static com.palangwi.soup.utils.ApiUtils.success;
+
+import com.palangwi.soup.dto.keyword.KeywordResponseDto;
+import com.palangwi.soup.service.KeywordService;
+import com.palangwi.soup.utils.ApiUtils.ApiResult;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class KeywordController {
+
+    private final KeywordService keywordService;
+
+    @GetMapping("/api/v1/keywords")
+    public ApiResult<KeywordResponseDto> getKeywords() {
+        return success(keywordService.getKeywords());
+    }
+}
