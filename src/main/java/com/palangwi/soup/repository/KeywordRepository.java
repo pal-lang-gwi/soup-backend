9d8d9aeb08bac2d7a397106cd106fb1a9011607c
유재광 <marmong9770@gmail.com>
2025-04-22 22:35:38 +0900

    fix: unnecessary methods and add optional type for dao

--- src/main/java/com/palangwi/soup/repository/KeywordRepository.java
+++ src/main/java/com/palangwi/soup/repository/KeywordRepository.java
@@ -1,10 +1,11 @@
 package com.palangwi.soup.repository;
 
 import com.palangwi.soup.domain.keyword.Keyword;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface KeywordRepository extends JpaRepository<Keyword, Long> {
     boolean existsByName(String name);
 
-    Keyword findByName(String name);
+    Optional<Keyword> findByName(String name);
 }
--- src/main/java/com/palangwi/soup/service/keyword/KeywordService.java
+++ src/main/java/com/palangwi/soup/service/keyword/KeywordService.java
@@ -4,8 +4,6 @@ import com.palangwi.soup.dto.keyword.KeywordResponseDto;
 
 public interface KeywordService {
 
-    KeywordResponseDto getKeywords();
-
     KeywordResponseDto getKeywordByName(String name);
 
     KeywordResponseDto createKeyword(String name);
