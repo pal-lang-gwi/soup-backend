974b7d14193c49d1672161db0bd277e3c7b77365
유재광 <marmong9770@gmail.com>
2025-04-23 22:41:57 +0900

    refactor: enhance Keyword entity with source and lifecycle flags

--- src/main/java/com/palangwi/soup/domain/keyword/Keyword.java
+++ src/main/java/com/palangwi/soup/domain/keyword/Keyword.java
@@ -7,13 +7,15 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Where;
 
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @Table(name = "keyword")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Keyword extends BaseEntity {
 
     @Id
@@ -23,4 +25,29 @@ public class Keyword extends BaseEntity {
     private String name;
 
     private String normalizedName;
+
+    private Source source;
+
+    private boolean visible;
+
+    private boolean deleted;
+
+    public static Keyword generateKeyword(String name, String normalizedName, Source source) {
+        return Keyword.builder()
+                .name(name)
+                .normalizedName(normalizedName)
+                .source(source)
+                .visible(true)
+                .deleted(false)
+                .build();
+    }
+
+    @Builder
+    private Keyword(String name, String normalizedName, Source source, boolean visible, boolean deleted) {
+        this.name = name;
+        this.normalizedName = normalizedName;
+        this.source = source;
+        this.visible = visible;
+        this.deleted = deleted;
+    }
 }
