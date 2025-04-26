74b91dc21ccc9dd2a4bbb86eb4c9e933f24f62a9
김여준 <kt65346337@gmail.com>
2025-04-26 18:12:45 +0900

    feat: 사용자와 키워드 간 중간테이블 생성

--- src/main/java/com/palangwi/soup/domain/keyword/Keyword.java
+++ src/main/java/com/palangwi/soup/domain/keyword/Keyword.java
@@ -1,11 +1,16 @@
 package com.palangwi.soup.domain.keyword;
 
 import com.palangwi.soup.domain.BaseEntity;
+import com.palangwi.soup.domain.userkeyword.UserKeyword;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -23,4 +28,7 @@ public class Keyword extends BaseEntity {
     private String name;
 
     private String normalizedName;
+
+    @OneToMany(mappedBy = "keyword", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<UserKeyword> userKeywords = new ArrayList<>();
 }
