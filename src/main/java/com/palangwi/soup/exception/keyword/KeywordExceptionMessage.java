c239748549cf8be872dfcddbfb30a42fdb31cdab
김여준 <kt65346337@gmail.com>
2025-04-28 11:49:28 +0900

    refactor: 테스트 코드 가독성 개선 및 키워드 에러명 일관성 있게 변경

--- src/main/java/com/palangwi/soup/exception/keyword/KeywordExceptionMessage.java
+++ src/main/java/com/palangwi/soup/exception/keyword/KeywordExceptionMessage.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum KeywordExceptionMessage {
     KEYWORD_NOT_FOUND("해당 키워드가 존재하지 않습니다.", HttpStatus.NOT_FOUND),
-    NOT_SUBSCRIBED_EXCEPTION("구독중인 키워드가 아닙니다.", HttpStatus.BAD_REQUEST);
+    NOT_SUBSCRIBED_KEYWORD("구독중인 키워드가 아닙니다.", HttpStatus.BAD_REQUEST);
 
     private final String message;
     private final HttpStatus status;
