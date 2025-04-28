55724d26f146edaf5217fd1dce22a0284ffdc52d
김여준 <kt65346337@gmail.com>
2025-04-28 16:13:00 +0900

    feat; Refresh Token 추가 및 jwt 쿠키로 전송하는 기능 추가

---
+++ src/main/java/com/palangwi/soup/service/user/RefreshTokenService.java
@@ -0,0 +1,32 @@
+package com.palangwi.soup.service.user;
+
+import com.palangwi.soup.repository.RefreshTokenRedisRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RefreshTokenService {
+
+    private final RefreshTokenRedisRepository refreshTokenRedisRepository;
+
+    @Value("${security.jwt.refresh-token.ttl}")
+    private long refreshTokenTtl;
+
+    public void saveRefreshToken(String refreshToken, Long userId) {
+        refreshTokenRedisRepository.save(refreshToken, userId, refreshTokenTtl);
+    }
+
+    public Long validateRefreshToken(String refreshToken) {
+        String userIdStr = refreshTokenRedisRepository.findUserIdByRefreshToken(refreshToken);
+        if (userIdStr == null) {
+            throw new RuntimeException("Invalid or expired Refresh Token");
+        }
+        return Long.valueOf(userIdStr);
+    }
+
+    public void invalidateRefreshToken(String refreshToken) {
+        refreshTokenRedisRepository.delete(refreshToken);
+    }
+}
