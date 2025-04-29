f3425580bff3c53067f404ff13d2e5dfc32cb35f
김여준 <kt65346337@gmail.com>
2025-04-29 21:45:34 +0900

    fix: Security에서 필터링하는 엔드포인트 수정 및 테스트 코드 수정

--- src/main/java/com/palangwi/soup/security/JwtAuthentication.java
+++ src/main/java/com/palangwi/soup/security/JwtAuthentication.java
@@ -1,11 +1,4 @@
 package com.palangwi.soup.security;
 
-import java.security.Principal;
-
-public record JwtAuthentication(Long id) implements Principal {
-
-    @Override
-    public String getName() {
-        return id.toString(); // 또는 적절한 String 반환
-    }
+public record JwtAuthentication(Long id) {
 }
