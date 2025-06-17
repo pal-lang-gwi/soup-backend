4cf06c18cfd80dd2a6c1828aa91e82319b9240c3
유재광 <marmong9770@gmail.com>
2025-06-17 16:08:24 +0900

    feat: AWS S3 설정 추가

--- src/main/java/com/palangwi/soup/config/SecurityConfig.java
+++ src/main/java/com/palangwi/soup/config/SecurityConfig.java
@@ -13,9 +13,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import jakarta.servlet.http.HttpServletResponse;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Configuration
