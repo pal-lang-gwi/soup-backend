50526611df46daf0e281acf6405f21dc09544fcc
유재광 <marmong9770@gmail.com>
2025-06-23 20:09:59 +0900

    feat: add findSubscribedKeywordIds query in repository

--- src/main/java/com/palangwi/soup/repository/userkeyword/UserKeywordRepository.java
+++ src/main/java/com/palangwi/soup/repository/userkeyword/UserKeywordRepository.java
@@ -6,6 +6,7 @@ import com.palangwi.soup.domain.userkeyword.UserKeyword;
 import java.util.List;
 import java.util.Optional;
 
+import java.util.Set;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -28,5 +29,5 @@ public interface UserKeywordRepository extends JpaRepository<UserKeyword, Long> 
 
     Page<UserKeyword> findAllByUser_IdAndSubscribedTrue(Long userId, Pageable pageable);
 
-    boolean existsByUserAndKeyword(User user, Keyword keyword);
+    Set<Long> findSubscribedKeywordIds(Long userId, List<Long> keywordIds);
 }
