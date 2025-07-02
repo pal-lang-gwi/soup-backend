34f0fc345f8087a563ec1b497905fadebed0a9ca
김여준 <kt65346337@gmail.com>
2025-07-02 21:32:26 +0900

    fix: 쿼ë¦ 파라미터 명시적으로 변경

--- src/main/java/com/palangwi/soup/repository/admin/keyword/AdminKeywordRepository.java
+++ src/main/java/com/palangwi/soup/repository/admin/keyword/AdminKeywordRepository.java
@@ -6,12 +6,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AdminKeywordRepository extends JpaRepository<PendingKeywordRequest, Long> {
     @Query("SELECT p FROM PendingKeywordRequest p WHERE p.keyword.status = :status")
-    Page<PendingKeywordRequest> findByStatus(Status status, Pageable pageable);
+    Page<PendingKeywordRequest> findByStatus(@Param("status") Status status, Pageable pageable);
 
     void deleteByKeywordId(Long id);
 }
