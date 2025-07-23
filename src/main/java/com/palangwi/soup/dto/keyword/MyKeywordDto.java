d1a884bd0142bf11f50b960b3d59aa7bea79a0ba
유재광 <gwang5630#daum.net>
2025-07-23 13:27:38 +0900

    feat: Add keywordId to MyKeywordDto for my keywords API response

--- src/main/java/com/palangwi/soup/dto/keyword/MyKeywordDto.java
+++ src/main/java/com/palangwi/soup/dto/keyword/MyKeywordDto.java
@@ -1,10 +1,20 @@
 package com.palangwi.soup.dto.keyword;
 
 import com.palangwi.soup.domain.userkeyword.UserKeyword;
+
 import java.time.LocalDateTime;
 
-public record MyKeywordDto(Long userId, String keyword, String normalizedKeyword, LocalDateTime registeredDate) {
+public record MyKeywordDto(Long userId,
+                           Long keywordId,
+                           String keyword,
+                           String normalizedKeyword,
+                           LocalDateTime registeredDate) {
     public static MyKeywordDto of(UserKeyword userKeyword) {
-        return new MyKeywordDto(userKeyword.getUser().getId(), userKeyword.getKeyword().getName(), userKeyword.getKeyword().getNormalizedName(), userKeyword.getLastModifiedDate());
+        return new MyKeywordDto(
+                userKeyword.getUser().getId(),
+                userKeyword.getKeyword().getId(),
+                userKeyword.getKeyword().getName(),
+                userKeyword.getKeyword().getNormalizedName(),
+                userKeyword.getLastModifiedDate());
     }
 }
