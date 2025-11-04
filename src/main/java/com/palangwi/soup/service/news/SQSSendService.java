48a0941536044291885c50131473086401599ecf
yeojunKim <kt65346337@gmail.com>
2025-11-04 16:31:29 +0900

    feat: SQS 적용

--- src/main/java/com/palangwi/soup/service/news/SQSSendService.java
+++ src/main/java/com/palangwi/soup/service/news/SQSSendService.java
@@ -21,40 +21,24 @@ public class SQSSendService {
     @Value("${cloud.aws.sqs.queue-name}")
     private String queueName;
 
-    @Value("${cloud.aws.sqs.queue-type}")
-    private String queueType;
-
     public SendResult<String> sendMessage(Long keywordId, String keywordName) {
         try {
-            String keywordIdToString = keywordId.toString();
-
             ObjectMapper objectMapper = new ObjectMapper();
-            Map<String, String> messageMap = new HashMap<>();
-            messageMap.put("keywordId", keywordIdToString);
-            messageMap.put("keywordName", keywordName);
+            Map<String, String> messageMap = Map.of(
+                    "keywordId", keywordId.toString(),
+                    "keywordName", keywordName
+            );
 
             String message = objectMapper.writeValueAsString(messageMap);
 
-            log.info(
-                    String.format("Send : %s 키워드 뉴스의 핵심 키워드 추출을 시작합니다. AI서버로 정보를 전달합니다: (키워드 ID : %s)",
-                            keywordName, keywordId));
+            log.info("Send: {} 키워드 뉴스의 핵심 키워드 추출을 시작합니다. (키워드 ID: {})", keywordName, keywordId);
 
-            if(queueType.equals("fifo")){
-                //FIFO인 경우 메시지에 groupID와 DeduplicationID 지정
-                return template.send(to -> to
-                        .queue(queueName)
-                        .messageGroupId(keywordIdToString)
-                        .messageDeduplicationId(keywordIdToString)
-                        .payload(message));
-            }
-            else{
-                return template.send(to -> to
-                        .queue(queueName)
-                        .payload(message));
-            }
+            return template.send(to -> to
+                    .queue(queueName)
+                    .payload(message));
+
         } catch (JsonProcessingException e) {
             throw new RuntimeException("JSON 직렬화 실패", e);
         }
-
     }
 }
